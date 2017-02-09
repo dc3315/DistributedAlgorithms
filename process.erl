0f57748a6c99df3d860d6e5d2b0e211188ca43f4
David Cattle <dc3314@cloud-vm-45-211>
2017-02-09 17:39:52 +0000

    Initialised maps

--- process.erl
+++ process.erl
@@ -16,21 +16,26 @@ task1(MaxMessages, Time, Processes) ->
   T = spawn(timer, start, []),
   T ! {bind, self(), Time},
   % Start broadcasting and receiving.
-  io:format("OK!~n"),
-  task1Helper(MaxMessages, Processes, #{}, #{}, 0).
+  % Initialise From map.
+  From = maps:from_list([{Process, 0} || Process <- Processes]), 
+  To = maps:from_list([{Process, 0} || Process <- Processes]),
+  task1Helper(MaxMessages, Processes, From, To, 0).
 
 task1Helper(MaxMessages, Processes, From, To, CurrentCount) ->
   % if MaxMessage is > CurrentCount:
   % block here.
   % otherwise
-  io:format("OK TASK1HELPER~n"),
+  %io:format("OK TASK1HELPER~n"),
   receive
-    {message, Sender} -> updateMap();
+    {message, Sender} -> ok;
+      %NewFrom = incrementMappedValue(From, Sender),
+      %task1Helper(MaxMessages, Processes, NewFrom, To, CurrentCount);
     {timeout} -> halt()
   after
-    0 -> broadcast(Processes, MaxMessages, CurrentCount)
+    0 -> halt() %broadcast(Processes, MaxMessages, CurrentCount)
+      %NewTo = [ Process ! {message, self()} || Process <- Processes ]
+
   end.
 
-updateMap() -> ok.
 broadcast(P, M, C) -> halt().
 log() -> ok.
--- timer.erl
+++ timer.erl
@@ -7,5 +7,5 @@ start() ->
   end.
 
 next(Process, Time) ->
-  io:format("TIMER OK~n"),
+  %io:format("TIMER OK~n"),
   timer:send_after(Time, Process, {timeout}).
