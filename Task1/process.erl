3e04735cd9f5859145f5d2291fed953ee0a5127e
David Cattle <dc3314@cloud-vm-45-211>
2017-02-12 12:18:07 +0000

    Final refactor of task1

--- Task1/process.erl
+++ Task1/process.erl
@@ -4,57 +4,57 @@
 
 start() -> 
   receive
-    {bindSystem, Processes} -> next(Processes)
+    {bindSystem, ProcessPIDs} -> next(ProcessPIDs)
   end.
 
 
-next(Processes) ->
+next(ProcessPIDs) ->
   receive
-    {task1, start, MaxMessages, Time, System, SelfToken} ->
-        task1(MaxMessages, Time, Processes, System, SelfToken)
+    {task1, start, MaxMessages, Time, SystemPID, SelfToken} ->
+        task1(MaxMessages, Time, ProcessPIDs, SystemPID, SelfToken)
   end.
 
 
-task1(MaxMessages, Time, Processes, System, SelfToken) -> 
+task1(MaxMessages, Time, ProcessPIDs, SystemPID, SelfToken) -> 
   % Send a timeout after Time ms.
   timer:send_after(Time, timeout),
   % Initialise From and To maps.
-  From = maps:from_list([{Process, 0} || Process <- Processes]), 
-  To = maps:from_list([{Process, 0} || Process <- Processes]),
+  From = maps:from_list([{ProcessPID, 0} || ProcessPID <- ProcessPIDs]), 
+  To = maps:from_list([{ProcessPID, 0} || ProcessPID <- ProcessPIDs]),
   % Start sending/listening.
   if 
     MaxMessages == 0 ->
-        task1Helper(infinity, Processes, From, To, 0, System, SelfToken);
+        task1Helper(infinity, ProcessPIDs, From, To, 0, SystemPID, SelfToken);
     true ->
-        task1Helper(MaxMessages, Processes, From, To, 0, System, SelfToken)
+        task1Helper(MaxMessages, ProcessPIDs, From, To, 0, SystemPID, SelfToken)
   end.
 
 
-task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, SelfToken) ->
+task1Helper(MaxMessages, ProcessPIDs, From, To, CurrentCount, SystemPID, SelfToken) ->
   receive
     timeout ->        
         % Create a formatted string, and print it.
         Vals = lists:flatten([io_lib:format("{~p,~p} ", 
-                [maps:get(Key, To), maps:get(Key, From)]) || Key <- Processes]),
+                [maps:get(Key, To), maps:get(Key, From)]) || Key <- ProcessPIDs]),
         io:format(io_lib:format("~p: ", [SelfToken]) ++ Vals ++ io_lib:format("~n", [])),
         % Notify the system.
-        System ! done;
-    {message, Sender} ->
+        SystemPID ! done;
+    {up, SenderPID} ->
         % Update received count.
-        NewFrom = maps:update(Sender, maps:get(Sender, From) + 1, From), 
-        task1Helper(MaxMessages, Processes, NewFrom, To, CurrentCount, System, SelfToken)
+        NewFrom = maps:update(SenderPID, maps:get(SenderPID, From) + 1, From), 
+        task1Helper(MaxMessages, ProcessPIDs, NewFrom, To, CurrentCount, SystemPID, SelfToken)
   after
     0 ->
       if 
         % If you can, broadcast.
         CurrentCount < MaxMessages ->
-            NewTo = incrementMapValuesFromKeyList(To, Processes),
-            [Process ! {message, self()} || Process <- Processes],
-            task1Helper(MaxMessages, Processes, From, NewTo, 
-                        CurrentCount + 1, System, SelfToken); 
+            NewTo = incrementMapValuesFromKeyList(To, ProcessPIDs),
+            [ProcessPID ! {up, self()} || ProcessPID <- ProcessPIDs],
+            task1Helper(MaxMessages, ProcessPIDs, From, NewTo, 
+                        CurrentCount + 1, SystemPID, SelfToken); 
         % Otherwise, just wait for the timeout.
         true ->
-            task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, SelfToken)
+            task1Helper(MaxMessages, ProcessPIDs, From, To, CurrentCount, SystemPID, SelfToken)
       end
   end.
 
