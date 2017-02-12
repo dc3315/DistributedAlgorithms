2609e8c465b0cdfe22322db8c4edd248670d021d
David Cattle <dc3314@cloud-vm-45-211>
2017-02-12 12:12:00 +0000

    Refactor process 1.

--- Task1/process.erl
+++ Task1/process.erl
@@ -10,12 +10,12 @@ start() ->
 
 next(Processes) ->
   receive
-    {task1, start, MaxMessages, Time, System, ID} ->
-        task1(MaxMessages, Time, Processes, System, ID)
+    {task1, start, MaxMessages, Time, System, SelfToken} ->
+        task1(MaxMessages, Time, Processes, System, SelfToken)
   end.
 
 
-task1(MaxMessages, Time, Processes, System, ID) -> 
+task1(MaxMessages, Time, Processes, System, SelfToken) -> 
   % Send a timeout after Time ms.
   timer:send_after(Time, timeout),
   % Initialise From and To maps.
@@ -24,25 +24,25 @@ task1(MaxMessages, Time, Processes, System, ID) ->
   % Start sending/listening.
   if 
     MaxMessages == 0 ->
-        task1Helper(infinity, Processes, From, To, 0, System, ID);
+        task1Helper(infinity, Processes, From, To, 0, System, SelfToken);
     true ->
-        task1Helper(MaxMessages, Processes, From, To, 0, System, ID)
+        task1Helper(MaxMessages, Processes, From, To, 0, System, SelfToken)
   end.
 
 
-task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, ID) ->
+task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, SelfToken) ->
   receive
     timeout ->        
         % Create a formatted string, and print it.
         Vals = lists:flatten([io_lib:format("{~p,~p} ", 
                 [maps:get(Key, To), maps:get(Key, From)]) || Key <- Processes]),
-        io:format(io_lib:format("~p: ", [ID]) ++ Vals ++ io_lib:format("~n", [])),
+        io:format(io_lib:format("~p: ", [SelfToken]) ++ Vals ++ io_lib:format("~n", [])),
         % Notify the system.
         System ! done;
     {message, Sender} ->
         % Update received count.
         NewFrom = maps:update(Sender, maps:get(Sender, From) + 1, From), 
-        task1Helper(MaxMessages, Processes, NewFrom, To, CurrentCount, System, ID)
+        task1Helper(MaxMessages, Processes, NewFrom, To, CurrentCount, System, SelfToken)
   after
     0 ->
       if 
@@ -51,10 +51,10 @@ task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, ID) ->
             NewTo = incrementMapValuesFromKeyList(To, Processes),
             [Process ! {message, self()} || Process <- Processes],
             task1Helper(MaxMessages, Processes, From, NewTo, 
-                        CurrentCount + 1, System, ID); 
+                        CurrentCount + 1, System, SelfToken); 
         % Otherwise, just wait for the timeout.
         true ->
-            task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, ID)
+            task1Helper(MaxMessages, Processes, From, To, CurrentCount, System, SelfToken)
       end
   end.
 
