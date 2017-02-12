2adb052e020db4ca7b20f0db575baf70fecadc46
David Cattle <dc3314@cloud-vm-45-211>
2017-02-12 11:53:38 +0000

    Final refactor of task2.

--- Task2/app.erl
+++ Task2/app.erl
@@ -3,49 +3,51 @@
 
 start() ->
     receive
-        {bindPL, PL, SelfID, N} -> 
-            task1(PL, SelfID, N)
+        {bindPL, PlPID, SelfToken, N} -> 
+            task1(PlPID, SelfToken, N)
     end.
 
     
-task1(PL, SelfID, N) -> 
+task1(PlPID, SelfToken, N) -> 
     receive
+        % Upon reception of the trigger, start the game.
         {pl_deliver, 0, {task1, start, MaxMessages, Time}} -> 
             timer:send_after(Time, {pl_deliver, -1, timeout}),
             From = maps:from_list([{Token, 0} || Token <- lists:seq(1, N)]), 
             To = maps:from_list([{Token, 0} || Token <- lists:seq(1, N)]),
-            if 
+            if
+                % Special case.
                 MaxMessages == 0 ->
-                    task1Helper(infinity, From, To, 0, PL, SelfID, N);
+                    task1Helper(infinity, From, To, 0, PlPID, SelfToken, N);
                 true ->
-                    task1Helper(MaxMessages, From, To, 0, PL, SelfID, N)
+                    task1Helper(MaxMessages, From, To, 0, PlPID, SelfToken, N)
             end
     end.
 
 
-task1Helper(MaxMessages, From, To, CurrentCount, PL, SelfID, N) -> 
+task1Helper(MaxMessages, From, To, CurrentCount, PlPID, SelfToken, N) -> 
     receive
-        {pl_deliver, FromToken, Message} -> 
-            
+        {pl_deliver, FromToken, Message} ->  
             case Message of
                 timeout -> 
+                    % Log and exit.
                     Vals = lists:flatten([io_lib:format("{~p,~p} ", 
                     [maps:get(Key, To), maps:get(Key, From)]) || Key <- lists:seq(1, N)]),
-                    io:format(io_lib:format("~p: ", [SelfID]) ++ Vals ++ io_lib:format("~n", [])),
-                    PL ! {pl_send, 0, done};
+                    io:format(io_lib:format("~p: ", [SelfToken]) ++ Vals ++ io_lib:format("~n", [])),
+                    PlPID ! {pl_send, 0, done};
                 up -> 
                     NewFrom = maps:update(FromToken, maps:get(FromToken, From) + 1, From),
-                    task1Helper(MaxMessages, NewFrom, To, CurrentCount, PL, SelfID, N)
+                    task1Helper(MaxMessages, NewFrom, To, CurrentCount, PlPID, SelfToken, N)
             end
     after
         0 ->
             if
                 CurrentCount < MaxMessages ->
-                    NewTo = incrementMapValuesFromKeyList(To, lists:seq(1,5)),
-                    [PL ! {pl_send, K, {message, SelfID, up}} || K <- lists:seq(1,N)],
-                    task1Helper(MaxMessages, From, NewTo, CurrentCount + 1, PL, SelfID, N);
+                    NewTo = incrementMapValuesFromKeyList(To, lists:seq(1, N)),
+                    [PlPID ! {pl_send, K, {message, SelfToken, up}} || K <- lists:seq(1,N)],
+                    task1Helper(MaxMessages, From, NewTo, CurrentCount + 1, PlPID, SelfToken, N);
                 true ->
-                    task1Helper(MaxMessages, From, To, CurrentCount, PL, SelfID, N)
+                    task1Helper(MaxMessages, From, To, CurrentCount, PlPID, SelfToken, N)
             end
     end.
 
