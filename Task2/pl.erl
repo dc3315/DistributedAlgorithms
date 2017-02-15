0c598f6aa3893f3d39c7b6b2461c63f8c9600c88
David Cattle <djpcattle@gmail.com>
2017-02-15 15:26:03 +0000

    Refactor task 2.

--- Task2/pl.erl
+++ Task2/pl.erl
@@ -16,11 +16,25 @@ next(AppPID, SystemPID) ->
             ready(PlMap, AppPID)
     end.
 
-% Get ready to transmit / deliver.
+% Scan the queue for any 'pl_send' messages, if there is one,
+% send it, then check for any deliveries, then recurse. 
+% This ensures that we give the process a chance to deliver messages
+% if it is flooded with send messages (from beb broadcasts).
 ready(PlMap, AppPID) ->
     receive
-        {pl_send, ToToken, Message} -> maps:get(ToToken, PlMap) ! Message;
-        {_, FromToken, Message} -> AppPID ! {pl_deliver, FromToken, Message}
-    end,
-    ready(PlMap, AppPID).
+        {pl_send, ToToken, Message} -> 
+            maps:get(ToToken, PlMap) ! {inter_pl, Message},
+            deliver(PlMap, AppPID)
+    after 0 ->
+        deliver(PlMap, AppPID)
+    end.
+
+deliver(PlMap, AppPID) ->
+    receive
+        {inter_pl, _Message} ->
+            AppPID ! {pl_deliver, _Message},
+            ready(PlMap, AppPID)
+    after 0 ->
+        ready(PlMap, AppPID)
+    end.
 
